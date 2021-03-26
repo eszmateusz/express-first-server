c20d9731f02d5da0474e653659d3eee2851fee57
eszmateusz <71881067+eszmateusz@users.noreply.github.com>
2021-03-26 20:53:00 +0100

    Created app layout + added darkStyle for 'about' page.

--- server.js
+++ server.js
@@ -4,44 +4,41 @@ const hbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('.hbs', hbs());
-app.set('view engine', '.hbs');
-
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/user', (req, res) => {
-  res.show('warning.html');
+  res.render('warning');
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
-});
-
-app.get(['/', '/home'] , (req, res) => {
-  res.show('index.html');
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' } );
 });
 
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '/style.css'));
+app.get('/contact', (req, res) => {
+  res.render('contact');
 });
 
-app.get('/test.png', (req, res) => {
-  res.sendFile(path.join(__dirname, '/test.png'));
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
+  res.status(404).send('404 not found...');
 })
 
 app.listen(8000, () => {
