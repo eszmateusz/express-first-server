776480a99c868efd56a81074570b283f02958499
eszmateusz <71881067+eszmateusz@users.noreply.github.com>
2021-03-26 19:01:44 +0100

    Add Handlebars.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use((req, res, next) => {
   res.show = (name) => {
@@ -14,6 +18,10 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/user', (req, res) => {
   res.show('warning.html');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.get(['/', '/home'] , (req, res) => {
